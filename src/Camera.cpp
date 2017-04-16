d38b20833bfa4a5dd2b14f130811591e557e3b60
Daniel Marcos <DanielM.B.Barbosa@hotmail.com>
2017-04-16 18:21:54 -0300

    Melhora movimento da câmera
    Algorítmo de movimento da câmera melhorado, mas ainda não está perfeito.

--- src/Camera.cpp
+++ src/Camera.cpp
@@ -14,26 +14,25 @@ void Camera::UnFollow(){
 
 void Camera::Update(float dt){
     if(focus != nullptr){
-        // Fazer do foco o centro da tela
+        // Fazer do centro da tela o foco
     }
+    
     else{
         if(INPUT_M.IsKeyDown(LEFT_ARROW_KEY) || INPUT_M.IsKeyDown(A_KEY)){
-            speed.x = dt;
+            speed.x += CAMERA_SPEED;
         }
-        else if(INPUT_M.IsKeyDown(UP_ARROW_KEY) || INPUT_M.IsKeyDown(W_KEY)){
-            speed.y = dt;
+        if(INPUT_M.IsKeyDown(UP_ARROW_KEY) || INPUT_M.IsKeyDown(W_KEY)){
+            speed.y += CAMERA_SPEED;
         }
-        else if(INPUT_M.IsKeyDown(RIGHT_ARROW_KEY) || INPUT_M.IsKeyDown(D_KEY)){
-            speed.x = dt * (-1);
+        if(INPUT_M.IsKeyDown(RIGHT_ARROW_KEY) || INPUT_M.IsKeyDown(D_KEY)){
+            speed.x += CAMERA_SPEED * (-1);
         }
-        else if(INPUT_M.IsKeyDown(DOWN_ARROW_KEY) || INPUT_M.IsKeyDown(S_KEY)){
-            speed.y = dt * (-1);
+        if(INPUT_M.IsKeyDown(DOWN_ARROW_KEY) || INPUT_M.IsKeyDown(S_KEY)){
+            speed.y += CAMERA_SPEED * (-1);
         }
-        else{
-            speed.x = 0;
-            speed.y = 0;
-        }
-        pos.x += speed.x * CAMERA_SPEED;
-        pos.y += speed.y * CAMERA_SPEED;
+        pos.x += speed.x * dt;
+        pos.y += speed.y * dt;
+        speed.x = 0;
+        speed.y = 0;
     }
 }
